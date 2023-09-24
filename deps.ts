7ed5b0b0f5d6677a75a94e21e5c4b3dbe27bd6cd
sgohlke <gohlke.stefan@yahoo.de>
2023-09-24 18:59:17 +0200

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,14 +1,14 @@
 export {
    Person,
    PersonService,
-} from 'https://raw.githubusercontent.com/sgohlke/ModPersonServiceDeno/1.0.17/personservice.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/ModPersonServiceDeno/1.0.18/personservice.ts'
 export {
    assertEquals,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/deps.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.3/deps.ts'
 
 export {
    logAndReturnErrorResponse,
    returnDataResponse,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/response/responseFunctions.ts'
-export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/response/ResponseHeaders.ts'
-export { startServer } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/server/serverFunctions.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.3/response/responseFunctions.ts'
+export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.3/response/ResponseHeaders.ts'
+export { startServer } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.3/server/serverFunctions.ts'
