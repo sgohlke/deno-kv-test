f8b5bca4f25feeb825b097da6d3aad4f559b12b8
sgohlke <gohlke.stefan@yahoo.de>
2024-02-05 22:11:19 +0100

    Update dependencies and unstable flags

--- deps.ts
+++ deps.ts
@@ -4,7 +4,7 @@ export {
 } from 'https://raw.githubusercontent.com/sgohlke/ModPersonServiceDeno/1.0.19/personservice.ts'
 export {
    assertEquals,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/deps.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-deps/main/deps.ts'
 
 export {
    FAVICON_SVG_STRING,
